28fc5f964216e377f45d770a4b5ffc92eb10cf63
Michael Raguso <michael.raguso@gmail.com>
2018-01-19 19:41:21 -0500

    finished webcam fun

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,114 @@
+// import { setInterval } from "timers";
+
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      // console.log(localMediaStream);
+      // set video source to localMediaStream
+      // need to convert localMediaStream, currently object, into a URL
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch((err) => {
+      // did not allow access to webcam
+      console.error('OH NO!!!', err);
+    });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  // setting canvas to video dimensions
+  canvas.width = width;
+  canvas.height = height;
+
+  // every 16 ms taking a frame from video playing
+  // returning setInterval so you can stop interval if needed to
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    // spits out massive array in order [red, green, blue, alpha, red, green, blue, alpha...]
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with pixels
+    // pixels = rgbSplit(pixels);
+
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.4; // ghosting effect
+
+    pixels = greenScreen(pixels);
+
+    // put pixels back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
+
+function takePhoto() {
+  // play the soun
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const dataURL = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = dataURL;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${dataURL}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+    pixels.data[i + 2] = pixels.data[i + 2] + 25; // blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // red
+    pixels.data[i + 100] = pixels.data[i + 1]; // green
+    pixels.data[i - 150] = pixels.data[i + 2]; // blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  // grab inputs and set values to level[input.name]
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out! by setting alpha below to 0
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
